00e253676e13f1abe8feaa31867e6b9ecad3816b
jayatithakkar <61868090+jayatithakkar@users.noreply.github.com>
2026-04-05 22:18:24 -0400

    Update Rsvp.java

--- src/main/java/com/rsvp/model/Rsvp.java
+++ src/main/java/com/rsvp/model/Rsvp.java
@@ -1,6 +1,7 @@
 package com.rsvp.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 /**
@@ -8,10 +9,61 @@ import java.util.List;
  * @version 1.0
  */
 @Data
+@NoArgsConstructor // REQUIRED by Firebase to read data
 public class Rsvp {
-
     private String id;
-    private int guestCount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGuestCount() {
+        return guestCount;
+    }
+
+    public void setGuestCount(Integer guestCount) {
+        this.guestCount = guestCount;
+    }
+
+    public List<String> getGuestNames() {
+        return guestNames;
+    }
+
+    public void setGuestNames(List<String> guestNames) {
+        this.guestNames = guestNames;
+    }
+
+    private String status;       // "yes" or "no"
+    private String name;         // For people who can't make it
+    private String email;        // For people attending
+    private Integer guestCount;  // Use Integer so it can be safely null
     private List<String> guestNames;
-    private String email;
 }
